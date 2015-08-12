3dfe1927dad1269ef08738a161b36e4b48dc64d6
Andrey Kachanov <andrey170749@yandex.ru>
2015-08-12 20:02:28 +0300

    Init GIT

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Box&Cube</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">s
+</head>
