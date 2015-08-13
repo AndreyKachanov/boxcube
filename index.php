a9532415d221098e53e4d789e60b5bd860da3794
Andrey Kachanov <andrey170749@yandex.ru>
2015-08-13 09:43:23 +0300

    Creating header

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head> 
-	<title>Box&Cube</title>
+	<title>Box&amp;Cube</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">s
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+<body>
+	<div class="container">
+		<header>
+			<div class="left_part"></div>
+			
+			<div class="center_part"></div>
+			<div class="right_part"></div>
+			<div class="bottom_part"></div>
+		</header>	
+	</div>
+</body>
