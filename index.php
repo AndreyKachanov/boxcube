d617874af8afaa3032e8d013713f649081e85172
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-04 12:07:12 +0300

    Adding top menu

--- index.php
+++ index.php
@@ -9,10 +9,16 @@
 	<div class="container vcard">
 		<header>
 			<div class="light_left_part">
-				<a class="logotip" href="index.php">
-					<h1 class="org">Box &amp; Cube</h1>
+				<a class="logo" href="index.php">
+					<h1 class="fn org">Box &amp; Cube</h1>
 					<h2 class="creative">create &amp; innovate solutions</h2>
 				</a>
+				<ul class="top_menu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Blog</a></li>
+				</ul>
 			</div>
 			<div class="light_right_part"></div>
 			<div class="light_center_part"></div>
