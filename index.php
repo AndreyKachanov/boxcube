fae204aa219bd141a0f0ee1573404a18228614d7
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-06 02:04:17 +0300

    Creating bottom menu

--- index.php
+++ index.php
@@ -357,6 +357,58 @@
 				</ul>
 				<div class="clear"></div>
 			</div>	
-		</main>	
+		</main>
+		<div class="bottom_menu">
+			<div class="about_us">
+				<h2>About Us</h2>
+				<ul>
+					<li><a href="#">Our Company</a></li>
+					<li><a href="#">Our Blog</a></li>
+					<li><a href="#">Submit a Site</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Help &amp; Terms</a></li>
+					<li><a href="#">Read Our FAQ</a></li>
+				</ul>
+			</div>
+			<div class="popular">
+				<h2>Popular Topics</h2>
+				<ul>
+					<li><a href="#">Trends &amp; Technology</a><span>(30)</span></li>
+					<li><a href="#">Design Companies</a><span>(2699)</span></li>
+					<li><a href="#">Design Freelancers</a><span>(23)</span></li>
+					<li><a href="#">Web Portfolios</a><span>(16)</span></li>
+					<li><a href="#">Web Development</a><span>(23)</span></li>
+					<li><a href="#">General Icons</a><span>(34)</span></li>
+				</ul>
+			</div>
+			<div class="follow">
+				<h2>Follow Us Here</h2>
+				<div class="social">
+					<a href="#"><img src="img/follow_us/1.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/2.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/3.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/4.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/5.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/6.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/7.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/8.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/9.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/10.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/11.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/12.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/13.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/14.png" alt=""></a>
+					<a href="#"><img src="img/follow_us/15.png" alt=""></a>
+				</div>
+			</div>
+			<div class="twitter">
+				<h2>Twitter Updates</h2>
+				<div class="bg">
+					<a class="white_color" href="#">@bogothemes</a> there is a new designer in town people!
+					<p>Check it out: <a href="#">http://bit.urds</a></p>
+				</div>
+			</div>
+			<br class="clear"></div>
+		</div>	
 	</div>
 </body>
